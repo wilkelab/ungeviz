32af2b71f04302380e3c9190fdafca755768eb74
Claus Wilke <wilke@austin.utexas.edu>
2018-12-20 15:09:13 -0600

    cleaned-up and improved sample_outcomes()

--- R/sample-outcomes.R
+++ R/sample-outcomes.R
@@ -5,6 +5,7 @@
 #'
 #' @examples
 #' library(mgcv)
+#' library(ggplot2)
 #' fit <- gam(mpg ~ s(disp), data = mtcars, method = "REML")
 #'
 #' newdata <- data.frame(
@@ -15,15 +16,39 @@
 #' )
 #'
 #' sample_df <- sample_outcomes(fit, newdata, 10, unconditional = TRUE)
+#' conf <- confidence_band(fit, newdata, unconditional = TRUE)
 #' ggplot(mtcars, aes(disp, mpg)) +
-#'  geom_point(color = "grey30", size = 0.5) +
-#'  geom_line(data = sample_df, aes(group = .draw), color = "#0072B2", size = 0.3)
-#'@export
-sample_outcomes <- function(model, newdata, samples = 20, ...) {
+#'   geom_ribbon(data = conf, aes(ymin = lo, ymax = hi), fill="#80808080", color = NA) +
+#'   geom_point(color = "grey30", size = 0.5) +
+#'   geom_line(data = sample_df, aes(group = .bootstrap), color = "#0072B2", size = 0.3) +
+#'   geom_line(data = conf, size = 1, color = "darkred")
+#'
+#' \dontrun{
+#' library(gganimate)
+#'
+#' data(BlueJays, package = "Stat2Data")
+#'
+#' fit <- gam(Skull ~ s(BillDepth) + s(BillWidth), data = BlueJays, method = "REML")
+#'
+#' newdata <- expand.grid(
+#'   BillWidth = seq(min(BlueJays$BillWidth), max(BlueJays$BillWidth), length.out = 20),
+#'   BillDepth = seq(min(BlueJays$BillDepth), max(BlueJays$BillDepth), length.out = 20)
+#' )
+#'
+#' sample_df <- sample_outcomes(fit, newdata, 30, unconditional = TRUE)
+#' ggplot(BlueJays, aes(BillDepth, BillWidth)) +
+#'   geom_tile(data = sample_df, aes(fill = Skull), alpha = 0.5, color = NA) +
+#'   geom_point(aes(size = Skull), color = "black", shape = 21, fill = NA) +
+#'   scale_fill_viridis_c(option = "D", guide = guide_legend(override.aes = list(alpha = 0.4))) +
+#'   theme_minimal() + #facet_wrap(~.bootstrap) # uncomment for static plot
+#'   transition_states(.bootstrap, 1, 0)
+#' }
+#' @export
+sample_outcomes <- function(model, newdata, times = 20, ...) {
   UseMethod("sample_outcomes", model)
 }
 
-#'@export
+#' @export
 sample_outcomes.default <- function(model, ...) {
   stop(
     "Function `sample_outcomes()` not implemented for objects of class '",
@@ -32,9 +57,10 @@ sample_outcomes.default <- function(model, ...) {
 }
 
 #'@export
-sample_outcomes.gam <- function(model, newdata, ndraws = 20, unconditional = FALSE) {
-  predictor <- model$pred.formula[[2]]
-  response <- model$terms[[2]]
+sample_outcomes.gam <- function(model, newdata, times = 20, unconditional = FALSE) {
+  # original code concept: Noam Ross
+  # https://gist.github.com/noamross/8bf1fc5b2f629b3a7e1eb6b4572e8388
+  response <- rlang::expr_name(attr(model$terms, "variables")[[2]])
 
   # Get the linear prediction matrix
   pred_mat <- predict(
@@ -48,12 +74,52 @@ sample_outcomes.gam <- function(model, newdata, ndraws = 20, unconditional = FAL
   vcov_mat <- vcov(model, unconditional = unconditional)
 
   # Draw 20 samples from the posterior and make predictions from them
-  coefs <- mvtnorm::rmvnorm(ndraws, mean = coef(model), sigma = vcov_mat)
+  coefs <- mvtnorm::rmvnorm(times, mean = coef(model), sigma = vcov_mat)
   preds <- pred_mat %*% t(coefs)
   pred_df <- as_tibble(preds) %>%
-    set_names(as.character(1:ndraws)) %>%
+    set_names(as.character(1:times)) %>%
     cbind(newdata) %>%
-    gather(.draw, !!response, -!!predictor)
+    gather(.bootstrap, !!response, 1:times)
 
   pred_df
+}
+
+
+#' @rdname sample_outcomes
+#' @export
+confidence_band <- function(model, newdata, level = 0.95, n = 100, ...) {
+  UseMethod("confidence_band", model)
+}
+
+#' @export
+confidence_band.default <- function(model, ...) {
+  stop(
+    "Function `confidence_band()` not implemented for objects of class '",
+    class(model), "'.", call. = FALSE
+  )
+}
+
+#' @export
+confidence_band.gam <- function(model, newdata, level = 0.95, n = 100, unconditional = FALSE) {
+  # original code concept: Noam Ross
+  # https://gist.github.com/noamross/8bf1fc5b2f629b3a7e1eb6b4572e8388
+  response <- as.symbol(rlang::expr_name(attr(model$terms, "variables")[[2]]))
+
+  # normal quantile corresponding to confidence level
+  std <- stats::qnorm(level / 2 + 0.5)
+
+  # predict confidence band
+  predict(
+    model,
+    newdata = newdata,
+    se.fit = TRUE,
+    unconditional = unconditional
+  ) %>%
+    as_tibble() %>%
+    cbind(newdata) %>%
+    rename(!!response := fit) %>%
+    mutate(
+      lo = !!response - std*se.fit,
+      hi = !!response + std*se.fit
+    )
 }
