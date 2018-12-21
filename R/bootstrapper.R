b5876000735ec1131acaad49270af6b799d2a2d5
Claus Wilke <wilke@austin.utexas.edu>
2018-12-21 17:29:13 -0600

    update readme and docs

--- R/bootstrapper.R
+++ R/bootstrapper.R
@@ -2,6 +2,7 @@
 #'
 #' @param times Number of independent bootstrap draws to perform.
 #' @param group An optional expression setting up the grouping to use for bootstrapping.
+#'   If not provided, any grouping present in the original dataset will be used.
 #' @param seed Random seed to use.
 #' @param key Name of the column that will hold an integer
 #'   running from 1 to `times` indicating the bootstrap replicate.
@@ -19,6 +20,29 @@
 #'   geom_point() +
 #'   geom_smooth(data = bootstrapper(5, Species), aes(group = .bootstrap), se = FALSE) +
 #'   facet_wrap(~Species)
+#'
+#' # it is important to set grouping correctly for bootstrapping
+#' set.seed(1234)
+#'
+#' df <- data.frame(
+#'   type = c(rep("A", 100), rep("B", 10), rep("C", 3)),
+#'   y = rnorm(113)
+#' )
+#'
+#' # incorrect: bootstrapping ungrouped dataset leads to missing category C
+#' ggplot(df, aes(type, y)) +
+#'   geom_pointrange(data = bootstrapper(6, seed = 562), stat = "summary") +
+#'   facet_wrap(~.bootstrap)
+#'
+#' # correct: bootstrapping within groups
+#' ggplot(df, aes(type, y)) +
+#'   geom_pointrange(data = bootstrapper(6, type, seed = 562), stat = "summary") +
+#'   facet_wrap(~.bootstrap)
+#'
+#' # also correct: use grouped data frame
+#' ggplot(group_by(df, type), aes(type, y)) +
+#'   geom_pointrange(data = bootstrapper(6, seed = 562), stat = "summary") +
+#'   facet_wrap(~.bootstrap)
 #' @export
 bootstrapper <- function(times, group = NULL, seed = NULL, key = ".bootstrap",
                          row = ".row", id = ".id", original_id = ".original_id") {
