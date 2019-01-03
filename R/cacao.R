fcb4389368e65fe7b526e64b56e20b48b63cb0ab
Claus Wilke <wilke@austin.utexas.edu>
2019-01-03 21:05:33 +0100

    vignette improvements

---
+++ R/cacao.R
@@ -0,0 +1,8 @@
+#' Ratings of chocolate bars
+#'
+#' Ratings of almost 1800 bars of chocolate, collected by Brady Brelinski,
+#' Founding Member of the Manhattan Chocolate Society.
+#'
+#' @references
+#' Manhattan Chocolate Society, \url{http://flavorsofcacao.com}
+"cacao"
